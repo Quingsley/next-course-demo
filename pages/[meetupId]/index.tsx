bd59e1bc0de332857a19f4ce48ffc47d2087584b
JEROME JUMAH <quingsley@hotmail.com>
2023-03-09 11:39:45 +0300

    Update index.tsx
    fixed fallback issue

--- pages/[meetupId]/index.tsx
+++ pages/[meetupId]/index.tsx
@@ -47,7 +47,7 @@ export async function getStaticPaths() {
     .toArray();
   client.close();
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => {
       return {
         params: {
